10024a864746b0445b4cc02690ac328fb226dc8d
Sarah <sarahswinton99@gmail.com>
2021-10-08 12:06:36 +0100

    Created src file

---
+++ src.m
@@ -0,0 +1,23 @@
+%---------------------------------------%
+% Main script for object-oriented MEng Refactor
+% Sarah Swinton 
+% 2243716S
+% Initially Created: 08/10/21
+%---------------------------------------%
+
+%% Instantiation of Required Classes 
+% rover{n} = typeOfRover(roverId, startPoint, targetPoint, desiredVelocity)
+rover{1} = activeRover();
+rover{2} = referenceRover(1,{1,1},0.1);
+
+rover{3} = activeRover(2,{2,1},0.1);
+rover{4} = referenceRover(2,{2,1},0.1);
+
+rover{5} = activeRover(3,{3,1},0.1); 
+rover{6} = referenceRover(3,{3,1},0.1); 
+
+rover{7} = activeRover(4,{4,1},0.1);
+rover{8} = referenceRover(4,{4,1},0.1);
+
+rover{9} = activeRover(5,{5,1},0.1); 
+rover{10} = referenceRover(5,{5,1},0.1); 
