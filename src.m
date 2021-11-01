33876c30b15ad93edb3ae846d287edf0f02d5753
Sarah <sarahswinton99@gmail.com>
2021-11-01 16:59:08 +0000

    Correct Instantiation of 5 Active and 5 Reference Rovers

--- src.m
+++ src.m
@@ -8,17 +8,17 @@ clear all
 
 %% Instantiation of Required Classes 
 % rover{n} = typeOfRover(roverId, startPoint, targetPoint, desiredVelocity)
-rover{1} = activeRover(1, [1, 1], [2, 2], 0.01);
-rover{2} = referenceRover(1, [1, 1], [2, 2], 0.01);
-% 
-% rover{3} = activeRover(2,{2,1},0.1);
-% rover{4} = referenceRover(2,{2,1},0.1);
-% 
-% rover{5} = activeRover(3,{3,1},0.1); 
-% rover{6} = referenceRover(3,{3,1},0.1); 
-% 
-% rover{7} = activeRover(4,{4,1},0.1);
-% rover{8} = referenceRover(4,{4,1},0.1);
-% 
-% rover{9} = activeRover(5,{5,1},0.1); 
-% rover{10} = referenceRover(5,{5,1},0.1); 
+rover{1} = activeRover(1, [1, 1], [1, 2], 0.01);
+rover{2} = referenceRover(1, [1, 1], [1, 2], 0.01);
+
+rover{3} = activeRover(2,[2,1],[2,2],0.1);
+rover{4} = referenceRover(2,[2,1],[2,2],0.1);
+
+rover{5} = activeRover(3,[3,1],[3,3],0.1); 
+rover{6} = referenceRover(3,[3,1],[3,3],0.1); 
+
+rover{7} = activeRover(4,[4,1],[4,4],0.1);
+rover{8} = referenceRover(4,[4,1],[4,4],0.1);
+
+rover{9} = activeRover(5,[5,1],[5,5],0.1); 
+rover{10} = referenceRover(5,[5,1],[5,5],0.1); 
