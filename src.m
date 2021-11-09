19e2eb99f64c932cfd50bc28918cd635ed6b001b
Sarah <sarahswinton99@gmail.com>
2021-11-09 11:32:42 +0000

    Debugged waypoint incrementation such that rover becomes inactive at final waypoint

--- src.m
+++ src.m
@@ -43,11 +43,16 @@ obsLocation(1,:) = 10;
 obsLocation(2,:) = 10;
 
 % Guidance
+% Store rover ativity status and time at onset of inactivity
+% roverInactive(n,1): 1 = Inactive, 0 = Active
+% roverInactive(n,2): timestep at onset of activity 
+roverInactive = zeros(length(rover),2);       
 
 %% Path Planning
 
 % Generate Waypoints
-waypoints(1) = [3,5,7;2,3,4];
+%waypoints = [1.5,3,5,7;1.5,2,3,4];
+waypoints = [2;1.5];
 
 % Assign Waypoints To Rovers 
     % Requried functionality: assignment of different waypoints arrays
@@ -59,86 +64,103 @@ end
 for time = 0:stepSize:endTime
     % Carry Out Simulation For Each Rover 
     for n = 1:1:length(rover)
-        %----------------------------------%
-        % Data Storage
-        if rem(stepSize,commsInterval) == 0
-            % increment counter after each rover has been processed for the
-            % current time step
-            if n == 1
-                i = i + 1;
+        % Only run the rover sim if the rover is active
+        if roverInactive(n,1) == 0
+            %----------------------------------%
+            % Data Storage
+            if rem(stepSize,commsInterval) == 0
+                % increment counter after each rover has been processed for the
+                % current time step
+                if n == 1
+                    i = i + 1;
+                end
+                % Store state 
+                stateOutput(:,i,n) = rover{n}.xo;
+                timeOutput(i,n) = time;
             end
-            % Store state 
-            stateOutput(:,i,n) = rover{n}.xo;
-            timeOutput(i,n) = time;
+            %----------------------------------%
+            
+    
+            %----------------------------------%
+            % LOS Navigation
+    
+            % Find the rover's current velocity
+            findVelocity(rover{n});
+            
+            % Find the distance between the rover and it's current waypoint
+            range = distanceToWaypoint(rover{n});
+            
+            % Check for any visible obstacles
+            visibleObstacles = checkForObstacles(rover{n},obsNumber,obsLocation);
+                      
+            % Find if either visible obs or the rover are within acceptance radius 
+            distanceToObs = zeros(length(visibleObstacles),1);
+            for j = 1:1:length(visibleObstacles)
+                xDelta = visibleObstacles(1)-rover{n}.waypoints(1,rover{n}.waypointCounter);
+                yDelta = visibleObstacles(2)-rover{n}.waypoints(2,rover{n}.waypointCounter);
+                obsRange = sqrt((xDelta)^2+(yDelta)^2);
+                distanceToObs(j) = obsRange;
+            end
+    
+            % Increment waypoint if necessary 
+            waypointIncrementer(rover{n}, range, min(distanceToObs)); 
+    
+            % Check if rover has reached its final waypoint
+            if rover{n}.waypointCounter > width(waypoints)
+                fprintf('Rover Number %i reached its final waypoint at time: %0.2f \n', n, time)
+                roverInactive(n,1) = 1;
+                roverInactive(n,2) = (time/stepSize)+1; 
+                break       
+            end
+            
+            % Find the rover's LOS Angle it's current waypoint
+            LOSAngle = findLOSAngle(rover{n});
+            
+            % Adjust the rover's LOS Angle to enable obstacle avoidance
+            LOSAngle = adjustForObstacles(rover{n}, visibleObstacles, LOSAngle);
+    
+            % Map the desired heading value
+            mapPsi(rover{n}, LOSAngle, stepSize);
+            %----------------------------------%
+    
+    
+            %----------------------------------%
+            % Control Section
+            headingControl(rover{n}, headingGains, stepSize);
+            velocityControl(rover{n}, velocityGains, stepSize);
+            u = [rover{n}.velCS;rover{n}.psiCS];
+            %----------------------------------%
+    
+    
+            %----------------------------------%
+            % Derivative Section 
+    
+            % rover{x}.xodot = roverModel(rover{n});
+            %----------------------------------%
+    
+    
+            %----------------------------------%
+            % Integral Section
+            rover{n}.xo = rk4int(rover{n},stepSize,u);
+            %----------------------------------%
         end
-        %----------------------------------%
-        
-
-        %----------------------------------%
-        % LOS Navigation
-
-        % Find the rover's current velocity
-        findVelocity(rover{n});
-        
-        % Find the distance between the rover and it's current waypoint
-        range = distanceToWaypoint(rover{n});
-        
-        % Check for any visible obstacles
-        visibleObstacles = checkForObstacles(rover{n},obsNumber,obsLocation);
-                  
-        % Find if either visible obs or the rover are within acceptance radius 
-        distanceToObs = zeros(length(visibleObstacles),1);
-        for j = 1:1:length(visibleObstacles)
-            xDelta = visibleObstacles(1)-rover{n}.waypoints(1,rover{n}.waypointCounter);
-            yDelta = visibleObstacles(2)-rover{n}.waypoints(2,rover{n}.waypointCounter);
-            obsRange = sqrt((xDelta)^2+(yDelta)^2);
-            distanceToObs(j) = obsRange;
-        end
-
-        % Increment waypoint if necessary 
-        waypointIncrementer(rover{n}, range, min(distanceToObs)); 
-        
-        % Find the rover's LOS Angle it's current waypoint
-        LOSAngle = findLOSAngle(rover{n});
-        
-        % Adjust the rover's LOS Angle to enable obstacle avoidance
-        LOSAngle = adjustForObstacles(rover{n}, visibleObstacles, LOSAngle);
-
-        % Map the desired heading value
-        mapPsi(rover{n}, LOSAngle, stepSize);
-        %----------------------------------%
-
-
-        %----------------------------------%
-        % Control Section
-        headingControl(rover{n}, headingGains, stepSize);
-        velocityControl(rover{n}, velocityGains, stepSize);
-        u = [rover{n}.velCS;rover{n}.psiCS];
-        %----------------------------------%
-
-
-        %----------------------------------%
-        % Derivative Section 
-
-        % rover{x}.xodot = roverModel(rover{n});
-        %----------------------------------%
-
-
-        %----------------------------------%
-        % Integral Section
-        rover{n}.xo = rk4int(rover{n},stepSize,u);
-        %----------------------------------%
-
     end
 end
 
 %% Terminal Segment 
+% Data Prep: Remove zeros from the end of the stateOutput Array
+% Note: last full row should be 9475
+for n = 1:1:width(rover)
+    lastFullColumn = roverInactive(n,2); 
+    stateOutput_1 = stateOutput(:,(1:lastFullColumn),n);
+end
+
 % Plotting Path of the First Rover
 clf
 figure(1)
 plot(waypoints(1,:),waypoints(2,:), "ko");
 hold on
-plot(stateOutput(7,:,1),stateOutput(8,:,1), "r-")
+plot(stateOutput_1(7,:,1),stateOutput_1(8,:,1), "r-")
 xlabel("X Position (m)")
 ylabel("Y Position (m)")
 legend("Desired Path","Measured Path")
