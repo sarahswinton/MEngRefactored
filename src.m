56d2a3ad40fbe745385d086febb9d85e6b75c096
Sarah <sarahswinton99@gmail.com>
2021-11-16 15:26:49 +0000

    Resolved: data storage after rover 1 completes path

--- src.m
+++ src.m
@@ -7,10 +7,10 @@ clear
 %---------------------------------------%
 %% Instantiation of Required Classes 
 % rover{n} = typeOfRover(roverId, startPoint, targetPoint, desiredVelocity, roverType)
-rover{1} = activeRover(1, [1, 1], [16, 10], 0.1, "Four Wheel");
-rover{2} = activeRover(1, [2, 1], [18, 10], 0.1, "Four Wheel");
-rover{3} = activeRover(1, [3, 1], [20, 10], 0.1, "Four Wheel");
-rover{4} = activeRover(1, [4, 1], [22, 10], 0.1, "Four Wheel");
+rover{1} = activeRover(1, [1, 1], [16, 20], 0.1, "Four Wheel");
+rover{2} = activeRover(1, [2, 1], [18, 20], 0.1, "Four Wheel");
+rover{3} = activeRover(1, [3, 1], [20, 20], 0.1, "Four Wheel");
+rover{4} = activeRover(1, [4, 1], [22, 20], 0.1, "Four Wheel");
 %rover{5} = activeRover(1, [5, 1], [22, 1], 0.1, "Four Wheel");
 
 % rover{2} = referenceRover(1, [1, 1], [1, 2], 0.01, "Four Wheel");
@@ -18,7 +18,7 @@ rover{4} = activeRover(1, [4, 1], [22, 10], 0.1, "Four Wheel");
 %% Simulation Initial Conditions
 stepSize = 0.01;            
 commsInterval = 0.01;       
-endTime = 650;   
+endTime = 400;   
 i = 0;
 
 % Data Output 
@@ -93,17 +93,17 @@ for roverNo = 1:1:length(rover)
     if roverNo ==1 
         [waypoints(1,:),waypoints(2,:)] = RRTStarOOP(rover{roverNo});
         assignWaypoints(rover{roverNo}, waypoints);
-        [plannedXOut(:,:),plannedPathLength(roverNo), plannedArrivalTime(roverNo)] = childRoverFcnOOP(waypoints(1,:),waypoints(2,:), rover{roverNo});
-        plannedPath{roverNo}.xLocation = plannedXOut(7,:);
-        plannedPath{roverNo}.yLocation = plannedXOut(8,:);
-        plannedXOut = [];
+%         [plannedXOut(:,:),plannedPathLength(roverNo), plannedArrivalTime(roverNo)] = childRoverFcnOOP(waypoints(1,:),waypoints(2,:), rover{roverNo});
+%         plannedPath{roverNo}.xLocation = plannedXOut(7,:);
+%         plannedPath{roverNo}.yLocation = plannedXOut(8,:);
+%         plannedXOut = [];
     else    
         [waypoints(1,:),waypoints(2,:)] = RRTStarOOP(rover{roverNo});
         assignWaypoints(rover{roverNo}, waypoints);
-        [plannedXOut(:,:),plannedPathLength(roverNo), plannedArrivalTime(roverNo)] = childRoverFcnOOP(waypoints(1,:),waypoints(2,:), rover{roverNo});
-        plannedPath{roverNo}.xLocation = plannedXOut(7,:);
-        plannedPath{roverNo}.yLocation = plannedXOut(8,:);
-        plannedXOut = [];
+%         [plannedXOut(:,:),plannedPathLength(roverNo), plannedArrivalTime(roverNo)] = childRoverFcnOOP(waypoints(1,:),waypoints(2,:), rover{roverNo});
+%         plannedPath{roverNo}.xLocation = plannedXOut(7,:);
+%         plannedPath{roverNo}.yLocation = plannedXOut(8,:);
+%         plannedXOut = [];
 %         for n = roverNo:-1:1
 % 
 %         end 
@@ -120,6 +120,9 @@ end
 for time = 0:stepSize:endTime
     % Carry Out Simulation For Each Rover 
     for n = 1:1:length(rover)
+        if n == 1
+            i = i + 1;
+        end
         % Only run the rover sim if the rover is active
         if roverInactive(n,1) == 0
             %----------------------------------%
@@ -127,9 +130,6 @@ for time = 0:stepSize:endTime
             if rem(stepSize,commsInterval) == 0
                 % increment counter after each rover has been processed for the
                 % current time step
-                if n == 1
-                    i = i + 1;
-                end
                 % Store state 
                 stateOutput(:,i,n) = rover{n}.xo;
                 timeOutput(i,n) = time;
@@ -166,39 +166,40 @@ for time = 0:stepSize:endTime
                 fprintf('Rover Number %i reached its final waypoint at time: %0.2f \n', n, time)
                 roverInactive(n,1) = 1;
                 roverInactive(n,2) = (time/stepSize)+1; 
-                break       
+                     
             end
-            
-            % Find the rover's LOS Angle it's current waypoint
-            LOSAngle = findLOSAngle(rover{n});
-            
-            % Adjust the rover's LOS Angle to enable obstacle avoidance
-            LOSAngle = adjustForObstacles(rover{n}, visibleObstacles, LOSAngle);
-    
-            % Map the desired heading value
-            mapPsi(rover{n}, LOSAngle, stepSize);
-            %----------------------------------%
-    
-    
-            %----------------------------------%
-            % Control Section
-            headingControl(rover{n}, headingGains, stepSize);
-            velocityControl(rover{n}, velocityGains, stepSize);
-            u = [rover{n}.velCS;rover{n}.psiCS];
-            %----------------------------------%
-    
-    
-            %----------------------------------%
-            % Derivative Section 
-    
-            % rover{x}.xodot = roverModel(rover{n});
-            %----------------------------------%
-    
-    
-            %----------------------------------%
-            % Integral Section
-            rover{n}.xo = rk4int(rover{n},stepSize,u);
-            %----------------------------------%
+            if roverInactive(n,1) == 0
+                % Find the rover's LOS Angle it's current waypoint
+                LOSAngle = findLOSAngle(rover{n});
+                
+                % Adjust the rover's LOS Angle to enable obstacle avoidance
+                LOSAngle = adjustForObstacles(rover{n}, visibleObstacles, LOSAngle);
+        
+                % Map the desired heading value
+                mapPsi(rover{n}, LOSAngle, stepSize);
+                %----------------------------------%
+        
+        
+                %----------------------------------%
+                % Control Section
+                headingControl(rover{n}, headingGains, stepSize);
+                velocityControl(rover{n}, velocityGains, stepSize);
+                u = [rover{n}.velCS;rover{n}.psiCS];
+                %----------------------------------%
+        
+        
+                %----------------------------------%
+                % Derivative Section 
+        
+                % rover{x}.xodot = roverModel(rover{n});
+                %----------------------------------%
+        
+        
+                %----------------------------------%
+                % Integral Section
+                rover{n}.xo = rk4int(rover{n},stepSize,u);
+                %----------------------------------%
+            end
         end
     end
 end
