9d300b799cd3cdd2934854e79f3852d151e29fc3
Sarah <sarahswinton99@gmail.com>
2021-12-06 14:59:21 +0000

    Removed Unneeded Files

--- Psi_Mapper_Corrected.m
+++ Psi_Mapper_Corrected.m
@@ -1,61 +1,59 @@
-function [accumulate,state] = Psi_Mapper_Corrected(psiWP,psiLast, state, xDelta, yDelta)
+function [accumulate,state] = Copy_of_Psi_Mapper_Corrected(psiNow,psiLast, state, xDelta, yDelta)
 % This function maps Psi from [-pi, pi] to [-inf,inf]
 
 % WP in First Quadrant (Unity Circle) 
 if (xDelta >= 0 && yDelta >= 0)
-    if(state==3) % use Guide Rule if in quadrant 3
-        if ((psiWP + abs(psiLast)) <= pi)
-            accumulate = psiWP - psiLast;
+    if(state==3) 
+        if ((psiNow + abs(psiLast)) <= pi)
+            accumulate = psiNow - psiLast;
         else
-            accumulate = psiWP - psiLast - 2*pi;
+            accumulate = psiLast - psiNow + 2*pi;
         end
     else 
-        accumulate = psiWP - psiLast;
+        accumulate = psiNow - psiLast;
     end
     state = 1;
 
 %WP in Second Quadrant (Unity Circle) 
-elseif (xDelta < 0 && yDelta >= 0)
+elseif (xDelta >= 0 && yDelta < 0)
     if(state==4) 
-        if ((psiWP + abs(psiLast)) <= pi)
-            accumulate = psiWP - psiLast;
+        if ((abs(psiNow) + psiLast) <= pi)
+            accumulate = psiNow - psiLast;
         else
-            accumulate = psiLast - psiWP - 2*pi; % potentially needs to be +ve
+            accumulate = psiNow - psiLast + 2*pi;
         end
-    elseif (state == 3)
-        accumulate = psiWP - psiLast + 2*pi;
-    elseif (state==2) 
-        accumulate = psiWP - psiLast;
     else 
-        accumulate = psiWP - psiLast;
+        accumulate = psiNow - psiLast;
     end
     state = 2;
 
 %WP in Third Quadrant (Unity Circle) 
 elseif (xDelta < 0 && yDelta < 0)
     if(state==1) 
-        if ((abs(psiWP) + psiLast) <= pi)
-            accumulate = psiWP - psiLast;
+        if ((abs(psiNow) + psiLast) <= pi)
+            accumulate = psiNow - psiLast;
         else
-            accumulate = psiWP - psiLast + 2*pi; % potentially +ve
+            accumulate = psiNow - psiLast + 2*pi; % potentially +ve
         end
-    elseif (state==2)
-        accumulate = psiWP - psiLast + 2*pi;
+    elseif (state==4)
+        accumulate = psiNow - psiLast + 2*pi;
     else
-        accumulate = psiWP - psiLast;
+        accumulate = psiNow - psiLast;
     end
     state = 3;
     
 %WP in Fourth Quadrant (Unity Circle) 
-else % (xDelta >= 0 && yDelta < 0)
-    if(state==2) 
-        if ((abs(psiWP) + psiLast) <= pi)
-            accumulate = psiWP - psiLast;
-        else
-            accumulate = psiWP - psiLast + 2*pi;
+elseif (xDelta < 0 && yDelta >= 0)
+    if (state == 2)
+        if (psiNow + abs(psiLast) <= pi)
+            accumulate = psiNow - psiLast;
+        else 
+            accumulate = psiLast - psiNow + 2*pi;
         end
+    elseif (state == 3)
+        accumulate = psiNow - psiLast - 2*pi; % potentially needs to be +ve
     else 
-        accumulate = psiWP - psiLast;
+        accumulate = psiNow - psiLast;
     end
     state = 4;
 end
